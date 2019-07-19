a485816260e26a8997fa5ca73f4bb80b608842f9
xialutiya <2770728609@qq.com>
2019-07-19 15:01:56 +0800

    提交

---
+++ src/main/java/com/itmuch/eureka/eureka0719001/Eureka0719001Application.java
@@ -0,0 +1,13 @@
+package com.itmuch.eureka.eureka0719001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eureka0719001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka0719001Application.class, args);
+    }
+
+}
